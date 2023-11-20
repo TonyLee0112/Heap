5ea24f08c3453b672669bbf09fc606bf4fce8564
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2023-11-20 14:18:44 +0900

    Update Min_heap.cpp

--- Min_heap.cpp
+++ Min_heap.cpp
@@ -21,8 +21,8 @@ private:
         return 0;
     }
 
-    void Insert(Heap & h, int key) {
-        int current_index = h.Heap_Size + 1;
+    void Insert(Heap & h, int key) { 
+        int current_index = h.Heap_Size + 1; // key 를 추가할 위치 = current_index & Array 의 마지막 원소
 
         if (h.Heap_Size == 0) {
             h.heap[current_index] = key;
@@ -37,13 +37,13 @@ private:
         h.heap[0] = h.Heap_Size;
 
         // 0 일때도 고려
-        while (current_index > 1) {
-            int parent_index = current_index / 2;
-            if (h.heap[parent_index] > key) {  // 부등호 방향이 변경됨
+        while (current_index > 1) { // Recursive
+            int parent_index = current_index / 2; //부모 node 의 index = 자식 node 의 index를 2로 나눈 몫
+            if (h.heap[parent_index] > key) {  // 부등호 방향이 변경됨 -> swap
                 int temp = h.heap[parent_index];
                 h.heap[parent_index] = h.heap[current_index];
                 h.heap[current_index] = temp;
-                current_index = parent_index;
+                current_index = parent_index; // Ex) [0,2,3,4,5,1] -> [0,2,1,4,5,3] -> [0,1,2,4,5,3] 
             }
                 // parent < key (부등호 방향이 변경됨)
             else {
@@ -53,19 +53,20 @@ private:
 
         return;
     }
+
     int Delete(Heap &h) {
 
-        if(HEAP.Heap_Size==1){
+        if(HEAP.Heap_Size == 1){
             int MIN_Key=h.heap[1];
             h.heap[1]=NULL;
-            h.heap[0]=0;
+            h.heap[0]=0; 
             h.Heap_Size=h.heap[0];
             return MIN_Key;
         }
 
 
         int MIN_Key = h.heap[1];  // Max Heap에서 Min Heap으로 수정됨
-        int Last_key = h.heap[h.Heap_Size];
+        int Last_key = h.heap[h.Heap_Size]; // heap_size != 원소의 개수 + 1, = 원소의 개수
         h.heap[h.Heap_Size] = 0;
         h.Heap_Size = h.Heap_Size - 1;
         h.heap[0] = h.Heap_Size;
@@ -78,13 +79,14 @@ private:
             int right_child = current_index * 2 + 1;
             int min_child = current_index; // 현재 노드와 비교할 노드
 
+            // 세 노드에 대하여 최소 값을 가지는 노드 탐색
             if (left_child <= h.Heap_Size && h.heap[left_child] < h.heap[min_child]) {  // 부등호 방향이 변경됨
                 min_child = left_child;
             }
             if (right_child <= h.Heap_Size && h.heap[right_child] < h.heap[min_child]) {  // 부등호 방향이 변경됨
                 min_child = right_child;
             }
-
+            // 발견된 최소 노드가 탐색 노드(부모 노드)가 아니면 swap
             if (min_child != current_index) {
                 int temp = h.heap[min_child];
                 h.heap[min_child] = h.heap[current_index];
@@ -95,9 +97,6 @@ private:
             }
         }
     }
-
-
-
 
 public:
     Min_Heap(){
@@ -115,6 +114,7 @@ public:
         cout << " 6. exit        " << endl;
         cout << endl;
     }
+
     void SelectMenu() {
         Display();
         int i ;
@@ -150,13 +150,6 @@ public:
             }
         }
     }
-
-
-
-
-
-
-
 
     void Insert_heap(){
         int key;
@@ -197,24 +190,10 @@ public:
         return;
 
     }
-
-
-
-
-
-
-
-
 };
 
-
-
-
 int main() {
-
     Min_Heap H;
     H.SelectMenu();
-
-
     return 0;
 }
